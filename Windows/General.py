078bae73e2cc90f0aa0818bd80d2033d215a5278
Adophilus <uchenna19of@gmail.com>
2019-11-24 16:30:44 +0100

    updated the general.py file for all Oses

--- Windows/General.py
+++ Windows/General.py
@@ -1,124 +1,115 @@
-import win32gui
-import win32con
 import json
 import time
 import requests
 import urllib
 from threading import Thread
-from re import search, sub, I
+from re import search, sub, I, escape
 
-class General ():
-	def __init__ (self):
-		pass
+def jsonize (string):
+	jsonData = json.loads(string)
+	return jsonData
 
-	def jsonize (self, string):
-		jsonData = json.loads(string)
-		return jsonData
-		
-	def load_json (self, path):
-		with open(path, "r") as file:
-			data = json.load(file)
+def unjsonize (json_data):
+	string = json.dumps(json_data, indent = 4)
+	return string
+	
+def load_json (path):
+	with open(path, "r") as file:
+		data = json.load(file)
 
-			return data
+		return data
 
-	def save_json (self, path, data):
-		with open(path, "w") as file:
-			json.dump(data, file, indent = 4)
+def save_json (path, data):
+	with open(path, "w") as file:
+		json.dump(data, file, indent = 4)
 
-	def json_save (self, path, data):
-		self.save_json(path, data)
+def json_save (path, data):
+	self.save_json(path, data)
 
-	def json_load (self, path = ""):
-		self.load_json(path)
+def json_load (path = ""):
+	self.load_json(path)
 
-	def hideWindow (self):
-		program = win32gui.GetForegroundWindow()
-		win32gui.ShowWindow(program, win32con.SW_HIDE)
+def putContentIn (filePath, data):
+	with open(filePath, "w") as file:
+		file.write(data)
 
-	def showWindow (self):
-		program = win32gui.GetForegroundWindow()
-		win32gui.ShowWindow(program, win32con.SW_SHOW)
+def getContentOf (filePath):
+	with open(filePath, "r") as file:
+		return file.read()
 
-	def putContentIn (self, filePath, data):
-		with open(filePath, "w") as file:
-			file.write(data)
+def getContentOfBinary (filePath):
+	with open(filePath, "rb") as file:
+		return file.read()
 
-	def getContentOf (self, filePath):
-		with open(filePath, "r") as file:
-			return file.read()
+def setImmediate (method, args = {}):
 
-	def setImmediate (self, method, args = {}):
+	def _method (method, arguments):
+		operation = method(**arguments)
 
-		def _method (method, arguments):
-			operation = method(**arguments)
+	immediateObj = Thread(target = _method, args = (method, args))
+	immediateObj.start()
+	return immediateObj
 
-		immediateObj = Thread(target = _method, args = (method, args))
-		immediateObj.start()
-		return immediateObj
+def setTimeout (method, secs = 5, args = {}):
 
-	def setTimeout (self, method, secs = 5, args = {}):
+	def _method (method, arguments, secs):
+		time.sleep(secs)
+		operation = method(**arguments)
 
-		def _method (method, arguments, secs):
+	timeoutObj = Thread(target = _method, args = (method, args, secs))
+	timeoutObj.start()
+	return timeoutObj
+
+def setInterval (method, secs = 10, args = {}):
+	def _method (method, arguments, secs):
+		while True:
 			time.sleep(secs)
 			operation = method(**arguments)
 
-		timeoutObj = Thread(target = _method, args = (method, args, secs))
-		timeoutObj.start()
-		return timeoutObj
+			if operation == "end" or operation == "break":
+				break
 
-	def setInterval (self, method, secs = 10, args = {}):
-		def _method (method, arguments, secs):
-			while True:
-				time.sleep(secs)
-				operation = method(**arguments)
+	intervalObj = Thread(target = _method, args = (method, args, secs))
+	intervalObj.start()
+	return intervalObj
 
-				if operation == "end" or operation == "break":
-					break
+def clearTimeout (timeoutObject):
+	pass
 
-		intervalObj = Thread(target = _method, args = (method, args, secs))
-		intervalObj.start()
-		return intervalObj
+def clearInterval (intervalObject):
+	self.clearTimeout(intervalObject)
 
-	def clearTimeout (self, timeoutObject):
-		pass
+def httpPost (url, postData = {}):
+	req = requests.post(url, data = postData)
 
-	def clearInterval (self, intervalObject):
-		self.clearTimeout(intervalObject)
+	return req.text
 
-	def httpPost (self, url, postData = {}):
-		req = requests.post(url, data = postData)
+def httpGet (url, getData = {}):
+	req = requests.get(url, data = postData)
 
-		return req.text
+	return req.text
 
-	def httpGet (self, url, getData = {}):
-		req = requests.get(url, data = postData)
+def exp (num1, num2):
+	return num1 ** num2
 
-		return req.text
+def swapQuotes (txt):
+	matchObj = search(r"'", txt, I)
+	if matchObj:
+		ntxt = sub(r"'", '"', txt)
+		return ntxt
+	else:
+		return txt
 
-	def exp (self, num1, num2):
-		return num1 ** num2
+def downloadFile (url, filename):
+	return urllib.urlretrieve(url, filename)
 
-	def swapQuotes (self, txt):
-		matchObj = search(r"'", txt, I)
-		if matchObj:
-			ntxt = sub(r"'", '"', txt)
-			return ntxt
-		else:
-			return txt
-
-	def wait (self, secs):
-		time.sleep(secs);
+def searchString (string, regex, ignoreCase = False):
+	if ignoreCase:
+		match = search(r"%s" % regex, string, I)
+	else:
+		match = search(r"%s" % regex, string)
 		
-	def downloadFile (self, url, filename):
-		return urllib.urlretrieve(url, filename)
-	
-	def searchString (self, string, regex, ignoreCase = False):
-		if ignoreCase:
-			match = search(r"%s" % regex, string, I)
-		else:
-			match = search(r"%s" % regex, string)
-			
-		return match
+	return match
 
-	def wait (self, secs):
-		time.sleep(secs)
+def wait (secs):
+	time.sleep(secs)
